cff03c293139a447cc67934dc1b328f0f07075bf
Your Name <bolot.muha@gmail.com>
2022-01-16 05:07:32 +0600

    feature/user/add_post&author&model

--- pinteresto/gallery/admin.py
+++ pinteresto/gallery/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Comment, Post, View
 
-# Register your models here.
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(View)
--- pinteresto/gallery/models.py
+++ pinteresto/gallery/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    image = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    author_post = models.CharField(max_length=70)
+    title = models.CharField(max_length=70)
+    text = models.TextField(blank=True)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    tag = models.CharField(max_length=50)
+
+
+class View(models.Model):
+    view = models.ForeignKey(Post, on_delete=models.CASCADE)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+
+class Comment(models.Model):
+    comment = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.CharField(max_length=255)
+    pub_date_comment = models.DateTimeField(auto_now_add=True)
+    author = models.CharField(max_length=70)
+
+
+
