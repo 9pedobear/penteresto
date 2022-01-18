86af04755b0c76e1f671a58106fac0ba611e593e
9pedobear <9pedobear@gmail.com>
2022-01-18 16:04:11 +0600

    feature/add_blog_and_category_models

--- pinteresto/gallery/urls.py
+++ pinteresto/gallery/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import *
+from django.conf.urls.static import static
+from pinteresto import settings
 
 urlpatterns = [
     path('', index, name='index'),
     path('post/', post, name='post'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
