9beed40296a1172d8f041616fa7f94bb2009fd80
9pedobear <9pedobear@gmail.com>
2022-01-21 16:01:56 +0600

    feature/add_forms_for_registration

--- user/urls.py
+++ user/urls.py
@@ -3,6 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('register/', register, name='register'),
-    path('login/', login, name='login'),
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
     path('users/',author, name='author'),
+    path('', HomeNews.as_view(), name='home'),
 ]
